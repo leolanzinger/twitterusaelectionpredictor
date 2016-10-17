2158b2f99f13dcb41efaa940db6a2294ae71dd9e
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2016-10-17 12:46:00 +0200

    heroku port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
 // set up twit
