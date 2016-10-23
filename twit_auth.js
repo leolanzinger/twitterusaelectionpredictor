df6828d026d48fa30671e895ef0b97295c5d01d2
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2016-10-23 15:07:09 +0200

    __

---
+++ twit_auth.js
@@ -0,0 +1,9 @@
+// twitter oauth keys go here
+module.exports = {
+  twitter_auth: {
+    consumer_key:         '',
+    consumer_secret:      '',
+    token:                '',
+    token_secret:         ''
+  }
+}
