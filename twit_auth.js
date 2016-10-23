28921fddac51a835ae6d47fae4135edd3f14d5eb
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2016-10-23 14:12:58 +0200

    _

--- twit_auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  twitter_auth: {
-    consumer_key:         'lpk43Aan4msGcxLgnml6y2cGC',
-    consumer_secret:      'eZtFNZ5H0BYgzZtGgDuHtDJYnqQ3fUcjNvqVyaVNS4SHYCDdrY',
-    token:         '65343092-Fpg8esH64gzezW6yhwhtuEAViL7lOQYzad3mNFNSM',
-    token_secret:  'EvSf88ivxrDgCdKn2pjJjAEispDGdVdC5oQBgkBArbtmd'
-  }
-}
