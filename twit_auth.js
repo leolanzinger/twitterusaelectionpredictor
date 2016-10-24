efda829f67b358aab02dd37e12a7a160e10c75bc
stenbln <77anteante77@gmail.com>
2016-10-24 16:25:58 +0200

    Added oAuth js file

---
+++ twit_auth.js
@@ -0,0 +1,10 @@
+// place your twitter app oAuth params here
+
+module.exports = {
+  twitter_auth: {
+    consumer_key:         'INSERT HERE',
+    consumer_secret:      'INSERT HERE',
+    token:         'INSERT HERE',
+    token_secret:  'INSERT HERE'
+  }
+}
